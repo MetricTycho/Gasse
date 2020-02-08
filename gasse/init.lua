293b6cbc1ab839e7abe1e376e342d9ea280ad60f
MetricTycho <46092636+MetricTycho@users.noreply.github.com>
2020-02-08 22:13:31 +0100

    Add files via upload

---
+++ gasse/init.lua
@@ -0,0 +1,20 @@
+minetest.register_node("gasse:oli1", {
+	tiles = {"oli1.png"},
+	description = ("Zauberstabregal 1"),
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},})
+
+minetest.register_node("gasse:oli2", {
+	tiles = {"oli2.png"},
+	description = ("Zauberstabregal 2"),
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},})
+
+minetest.register_node("gasse:oli3", {
+	tiles = {"oli3.png"},
+	description = ("Zauberstabregal 3"),
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},})
+
+minetest.register_node("gasse:kessel1", {
+	tiles = {"kessel1.png"},
+	description = ("Kesselregal"),
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},})
+
